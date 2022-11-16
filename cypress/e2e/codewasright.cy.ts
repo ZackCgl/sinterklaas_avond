f91855fcd7d2f7c1e2342b79b0475206d674d2a1
Mark Teekens <mark@rubyfinance.nl>
2022-11-16 21:57:29 +0100

    added env variables

--- cypress/e2e/codewasright.cy.ts
+++ cypress/e2e/codewasright.cy.ts
@@ -1,12 +1,24 @@
- 
 
-describe('the code was right', () => {
-  it('passes', () => {
+
+const RIGHT_CODE = "kapoentje"
+
+describe('check the output of entering an input as a code', () => {
+  it('empty', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('p').contains("Vul een wachtwoord in")
+  })
+  it('passes', async () => {
+    cy.visit('http://localhost:3000')
+    cy.get('input[name="inputCode"]').click
+    cy.get('input[name="inputCode"]').type(RIGHT_CODE)
+    cy.get('p').contains("Open de pakjes!")
+  })
+  it('failed', () => {
     cy.visit('http://localhost:3000')
     cy.get('input[name="inputCode"]').click
 
-    cy.get('input[name="inputCode"]').type("kapoentje")
-    cy.get('p').contains("Open de pakjes!")
+    cy.get('input[name="inputCode"]').type("fbnsdjbfhdshj")
+    cy.get('p').contains("Fout, geen kado!")
   })
 })
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,16 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import { useState } from 'react'
 
-
 const Home: NextPage = () => {
-  const [code, setCode] = useState<string>("")
-  
+  const RIGHT_CODE = "kapoentje"
+  const [inputCode, setInputCode] = useState<string>("")
+
   return (
-    <div className={`${code == "kapoentje" ? "bg-[url('https://thumbs.gfycat.com/WealthyOldfashionedDromaeosaur-size_restricted.gif')]" : 'bg-[url("https://media.tenor.com/M1z8DYZXVjkAAAAd/wallpaper.gif")] bg-cover'} bg-center min-h-screen h-full`}>
+    //check if the code is passed and change background
+    <div className={`${inputCode == RIGHT_CODE ? 
+    "bg-[url('https://thumbs.gfycat.com/WealthyOldfashionedDromaeosaur-size_restricted.gif')]" : 
+    'bg-[url("https://media.tenor.com/M1z8DYZXVjkAAAAd/wallpaper.gif")] bg-cover'} 
+    bg-center min-h-screen h-full`}>
       
       <Head>
         <title>Sinterklaas</title>
@@ -15,24 +19,37 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+
       <main className='flex justify-center'>
         <div className=' text-white font-bold text-2xl mt-10 font-serif flex flex-col justify-center ml-4 mr-4'>
-          {code != "kapoentje" && 
+          {inputCode != RIGHT_CODE && 
           <div>
             <h1 className='mb-4 mt-20'>Beste Suze,</h1>
             <p>De Sint heeft besloten dat je niet zo lief bent geweest.</p>
             <p>Om toegang te krijgen tot je kadootjes, moet je een wachtwoord raden.</p>
           </div>}
-          <div className='mt-10 flex- flex-col'>
-            <input value={code} name="inputCode" onChange={(e) => setCode(e.target.value)} type="password" className='text-black w-40'/>
-          </div>
+
+          {/*input password/code */}
+          <form className='mt-10 flex- flex-col'>
+            <input value={inputCode} name="inputCode" onChange={(e) => setInputCode(e.target.value)} 
+            type="password" className='text-black w-40'/>
+          </form>
+
+          {/*output */}
           <div className='mt-2 font-serif'>
-            {code == "kapoentje" && <p className='mt-10 text-4xl font-serif text-white'>Open de pakjes!</p>}
-            {code != "kapoentje" && code.length != 0 && <div><p className='text-red-500 mt-10'>Fout, geen kado!</p><img className='h-52 w-82' src="https://i.imgur.com/MHTsAUO.png" alt="" /></div>}
-            {code.length == 0 && <p className='text-blue-500'>Vul een wachtwoord in</p>}
+            {/*no input */}
+            {inputCode.length == 0 && <p className='text-blue-500'>Vul een wachtwoord in</p>}
+
+            {/*wrong input */}
+            {inputCode != RIGHT_CODE && inputCode.length != 0 && <div><p className='text-red-500 mt-10'>Fout, geen kado!</p>
+            <img className='h-52 w-82' src="https://i.imgur.com/MHTsAUO.png" alt="" /></div>}
+
+            {/*right input */}
+            {inputCode == RIGHT_CODE && <p className='mt-10 text-4xl font-serif text-white'>Open de pakjes!</p>}
+           
           </div>
         </div>
-        </main>
+      </main>
     </div>
   )
 }
