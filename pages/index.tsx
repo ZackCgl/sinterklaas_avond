aa2b9584226fa513f91eacc5fd30db6cc4a56474
Mark Teekens <mark@rubyfinance.nl>
2022-12-03 13:00:55 +0100

    add

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
     //check if the code is passed and change background
     <div className={`${inputCode == RIGHT_CODE ? 
     "bg-[url('https://thumbs.gfycat.com/WealthyOldfashionedDromaeosaur-size_restricted.gif')]" : 
-    'bg-[url("https://media.tenor.com/M1z8DYZXVjkAAAAd/wallpaper.gif")] bg-cover'} 
+    'bg-[url("https://i.imgur.com/jLcUw0M.gif")] bg-cover'} 
     bg-center min-h-screen h-full`}>
       
       <Head>
@@ -21,7 +21,7 @@ const Home: NextPage = () => {
 
 
       <main className='flex justify-center'>
-        <div className=' text-white font-bold text-2xl mt-10 font-serif flex flex-col justify-center ml-4 mr-4'>
+        <div className=' text-white font-bold text-2xl mt-28 font-serif flex flex-col justify-center ml-4 mr-4'>
           {inputCode != RIGHT_CODE && 
           <div>
             <h1 className='mb-4 mt-20'>Beste Suze,</h1>
@@ -38,14 +38,14 @@ const Home: NextPage = () => {
           {/*output */}
           <div className='mt-2 font-serif'>
             {/*no input */}
-            {inputCode.length == 0 && <p className='text-blue-500'>Vul het wachtwoord in</p>}
+            {inputCode.length == 0 && <p className='text-white'></p>}
 
             {/*wrong input */}
-            {inputCode != RIGHT_CODE && inputCode.length != 0 && <div><p className='text-red-500 mt-10'>Fout, geen kado!</p>
+            {inputCode != RIGHT_CODE && inputCode.length != 0 && <div><p className='text-white mt-10'>Fout, geen kado!</p>
             <img className='h-52 w-82' src="https://i.imgur.com/MHTsAUO.png" alt="" /></div>}
 
             {/*right input */}
-            {inputCode == RIGHT_CODE && <p className='mt-10 text-4xl font-serif text-white'>Goed gedaan!</p>}
+            
             {inputCode == RIGHT_CODE && <p className='mt-10 text-4xl font-serif text-white'>De pakjes liggen bij de jassen!</p>}
 
           </div>
